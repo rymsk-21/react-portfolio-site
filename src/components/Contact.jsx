4c65046d65aeefae9affc4f167588b4549e1f9b5
RyomaSeki <seki.r621@gmail.com>
2022-05-24 00:05:53 +0900

    fix

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+export const Contact = () => {
+    return(
+        <div id="contact" className="contact">
+            <div className="container">
+                <div className="contact-contents">
+                    <h2>Contact to Me!</h2>
+                    <p className="description">お仕事のご相談・依頼などは、下記フォームよりお問合せください</p>
+                    <a href="https://twitter.com/" className="btn">Form Push</a>
+                </div>
+            </div>
+        </div>
+    )
+}
